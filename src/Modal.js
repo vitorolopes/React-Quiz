03f80791a4360fca1b41832c0f37ca6daf887044
vitor lopes <lopesvitor959@gmail.com>
2022-04-09 19:15:24 +0100

    Initial Setup

---
+++ src/Modal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useGlobalContext} from './context'
+
+const Modal = () => {
+  return (
+    <div>Modal</div>
+  )
+}
+
+export default Modal
---
+++ src/SetupForm.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {useGlobalContext} from './context'
+
+const SetupForm = () => {
+  return (
+    <div>SetupForm</div>
+  )
+}
+
+export default SetupForm
